c92e6041e39e2e4e772c5ec52fea35962e039088
yagmurdal <yaagmurdaal@gmail.com>
2021-02-27 22:15:36 +0300

    catch error & dynamic bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     let keyWord = "İstanbul";
     let cityName;
     let temp;
@@ -10,7 +9,7 @@ $(document).ready(function () {
     $("#search-input input").keyup(function (e) {
         var code = e.key;
         if (code === "Enter") e.preventDefault();
-        if (code === " " || code === "Enter" || code === "," || code === ";") {
+        if (code === "Enter") {
             keyWord = $(this).val()
             $.ajax({
                 method: "GET",
@@ -21,15 +20,46 @@ $(document).ready(function () {
                     $("#city-name h3").text(response.location.name)
                     $("#city-temp .temp-number").text(response.current.temperature)
                     $("#feels-like .feels-number").text(response.current.feelslike)
+                    let time12h = response.current.observation_time
+                    // let [clock, timezone] = time.split(" ")
+                    // let [hour, min] = clock.split(":")
+                    let time24h = convertTime12to24(time12h)
+                    let [hour, min] = time24h.split(":")
+                    let app = $("#app")
+                    app.removeClass("night day")
+                    if (hour >= "20" && hour <= "4") {
+                        app.addClass("night")
+                    }else if (hour > "4" && hour < "15") {
+                        app.addClass("day")
+                    }
+                    debugger
                     // Fake request delay
                     setTimeout(() => {
                         $(".city-info-content").show()
                         $(".loader").hide()
+                        $(this).val("")
                     }, 1000);
+                }).catch(error => {
+                    swal("Upss!", `There is no city called ${keyWord}`, "error");
+                    $(".loader").hide()
                 })
         }
-
-
     })
 })
+
+const convertTime12to24 = (time12h) => {
+    const [time, modifier] = time12h.split(' ');
+
+    let [hours, minutes] = time.split(':');
+
+    if (hours === '12') {
+        hours = '00';
+    }
+
+    if (modifier === 'PM') {
+        hours = parseInt(hours, 10) + 12;
+    }
+
+    return `${hours}:${minutes}`;
+}
 
